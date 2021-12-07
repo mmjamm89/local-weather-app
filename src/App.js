e5cac4f25099313b2c05bd484450032b8d0003c5
Juan Martinez <78063884+mmjamm89@users.noreply.github.com>
2021-12-07 02:42:39 -0300

    app UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import './App.css';
+import './styles/App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-     <h1>Local weather</h1>
+     <Content />
     </div>
   );
+}
+
+const Content = () => {
+  return(
+    <div className="content">
+      <h3 class="city">Buenos Aires</h3>
+      <h1 className="temp">22</h1>
+      <h3 className="current-weather">Cloudy</h3>
+      <div className="current-img">
+        <i class="fas fa-cloud-moon-rain"></i>
+      </div>
+      <button className="degree-changer">C/F</button>
+    </div>
+  )
 }
 
 export default App;
