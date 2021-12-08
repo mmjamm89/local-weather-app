bba09c6c8d4c425af04f9781efd92131e8484505
Juan Martinez <78063884+mmjamm89@users.noreply.github.com>
2021-12-07 23:39:35 -0300

    axios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 import './styles/App.css';
 
+const key = "";
+
+const client = axios.create({
+  baseURL: ""
+});
+
 const App = () => {
+
+const [temp, setTemp] = useState(null);
+const [weather, setWeather] = useState(null);
+const [img, setImg] = useState(null)
+const [background, setBackground] = useState(null);
+
+// useEffect(() => {
+//   client.get(key).then(response => {
+//     setTemp(response.data)
+//   })
+// }, [])
+
   return (
     <div className="App">
      <Content />
@@ -11,11 +31,11 @@ const App = () => {
 const Content = () => {
   return(
     <div className="content">
-      <h3 class="city">Buenos Aires</h3>
+      <h3 className="city">Buenos Aires</h3>
       <h1 className="temp">22</h1>
       <h3 className="current-weather">Cloudy</h3>
       <div className="current-img">
-        <i class="fas fa-cloud-moon-rain" id="weather-img"></i>
+        <i className="fas fa-cloud-moon-rain" id="weather-img"></i>
       </div>
       <button className="degree-changer">C/F</button>
     </div>
