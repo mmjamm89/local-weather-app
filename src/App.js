6b6991c23abe49d04f6d712e6438d4fa260663a9
Juan Martinez <78063884+mmjamm89@users.noreply.github.com>
2021-12-08 23:38:53 -0300

    fetch weather API

--- src/App.js
+++ src/App.js
@@ -2,18 +2,34 @@ import {useState, useEffect} from 'react';
 import axios from 'axios';
 import './styles/App.css';
 
-const key = "";
+const api = {
+  key: "d3d157f5f2911c66ec500fac83b25c7e",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
-const client = axios.create({
-  baseURL: ""
-});
+
+// const key = "";
+
+// const client = axios.create({
+//   baseURL: ""
+// });
 
 const App = () => {
 
+const [city, setCity] = useState('Buenos Aires');
 const [temp, setTemp] = useState(null);
 const [weather, setWeather] = useState(null);
 const [img, setImg] = useState(null)
 const [background, setBackground] = useState(null);
+
+fetch(`${api.base}weather?q=${city}&units=metrics&APPID=${api.key}`)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+    setWeather('');
+    
+  })
+  
 
 // useEffect(() => {
 //   client.get(key).then(response => {
@@ -23,15 +39,15 @@ const [background, setBackground] = useState(null);
 
   return (
     <div className="App">
-     <Content />
+     <Content city={city}/>
     </div>
   );
 }
 
-const Content = () => {
+const Content = ({city}) => {
   return(
     <div className="content">
-      <h3 className="city">Buenos Aires</h3>
+      <h3 className="city">{city}</h3>
       <h1 className="temp">22</h1>
       <h3 className="current-weather">Cloudy</h3>
       <div className="current-img">
